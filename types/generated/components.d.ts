3e005de12afc23a6706e1a48f23ca056c31d1612
unknown <tamiloreolaniyi@gmail.com>
2024-12-02 12:05:43 +0100

    initial commit

---
+++ types/generated/components.d.ts
@@ -0,0 +1,69 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface EducationEducation extends Struct.ComponentSchema {
+  collectionName: 'components_education_educations';
+  info: {
+    displayName: 'Education';
+    icon: 'clock';
+  };
+  attributes: {
+    degree: Schema.Attribute.String;
+    description: Schema.Attribute.String;
+    endDate: Schema.Attribute.String;
+    major: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    universityName: Schema.Attribute.String;
+  };
+}
+
+export interface ExperienceExperience extends Struct.ComponentSchema {
+  collectionName: 'components_experience_experiences';
+  info: {
+    description: '';
+    displayName: 'Experience';
+    icon: 'calendar';
+  };
+  attributes: {
+    city: Schema.Attribute.String;
+    companyName: Schema.Attribute.String;
+    endDate: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    state: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+    workSummery: Schema.Attribute.RichText;
+  };
+}
+
+export interface SkillsSkills extends Struct.ComponentSchema {
+  collectionName: 'components_skills_skills';
+  info: {
+    displayName: 'Skills';
+    icon: 'plane';
+  };
+  attributes: {
+    name: Schema.Attribute.String;
+    rating: Schema.Attribute.Integer;
+  };
+}
+
+export interface ThemecolorThemeColor extends Struct.ComponentSchema {
+  collectionName: 'components_themecolor_theme_colors';
+  info: {
+    displayName: 'themeColor';
+    icon: 'shield';
+  };
+  attributes: {
+    themeColor: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'education.education': EducationEducation;
+      'experience.experience': ExperienceExperience;
+      'skills.skills': SkillsSkills;
+      'themecolor.theme-color': ThemecolorThemeColor;
+    }
+  }
+}
